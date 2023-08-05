f7682ef544ecf2515acd710865fa46f79c6151e8
mcshibbs <31636604+mcshibbs@users.noreply.github.com>
2023-08-04 22:26:15 -0400

    Update install.sh
    Added Brave Extensions

--- install.sh
+++ install.sh
@@ -109,11 +109,19 @@ nala install -y librewolf
 nala install -y codium
 nala install -y nextdns
 
-# Section - 13 | Purpose - Add Bitwarden Extension to Brave | # Linked Resources
-BITWARDEN_ID=nngceckbapebfimnlniiiahkandclblb
+# Section - 13 | Purpose - Add Bitwarden, Joplin, and KDE Plasma Extension to Brave | # Linked Resources
+## Creates path to place extensions
 EXTENSIONS_PATH=/opt/brave.com/brave/extensions
 mkdir -p $EXTENSIONS_PATH
+#
+BITWARDEN_ID=nngceckbapebfimnlniiiahkandclblb
 echo '{ "external_update_url": "https://clients2.google.com/service/update2/crx" }' > "${EXTENSIONS_PATH}/${BITWARDEN_ID}.json"
+#
+KDE_ID=cimiefiiaegbelhefglklhhakcgmhkai
+echo '{ "external_update_url": "https://clients2.google.com/service/update2/crx" }' > "${EXTENSIONS_PATH}/${KDE_ID}.json"
+#
+JOPLIN_ID=alofnhikmmkdbbbgpnglcpdollgjjfek
+echo '{ "external_update_url": "https://clients2.google.com/service/update2/crx" }' > "${EXTENSIONS_PATH}/${JOPLIN_ID}.json"
 
 # Section - 14 | Purpose - Install Joplin | Linked Resources - https://joplinapp.org/help/#desktop-applications
 #
